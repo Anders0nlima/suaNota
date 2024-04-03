9532f1513ed92c5904c62d132246ad52c7d5edd5
Anders0nlima <121578478+Anders0nlima@users.noreply.github.com>
2024-04-02 21:44:32 -0300

    up

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 function CalcularSimple(){
-    let linguagem = document.getElementById('linguagem')
-    let humanas = document.getElementById('humanas')
-    let natureza = document.getElementById('natureza')
-    let mate = document.getElementById('mate')
-    let redação = document.getElementById('redação')
+    let linguagem = document.getElementById('linguagem').value.trim();
+    let humanas = document.getElementById('humanas').value.trim();
+    let natureza = document.getElementById('natureza').value.trim();
+    let mate = document.getElementById('mate').value.trim();
+    let redação = document.getElementById('redação').value.trim();
     let resultado = document.getElementById('resultado')
     let img = document.createElement('img')
     img.setAttribute('id', 'foto')
 
-    let ling = Number(linguagem.value)
-    let hum = Number(humanas.value)
-    let nat = Number(natureza.value)
-    let mat = Number(mate.value)
-    let red = Number(redação.value)
+    if(linguagem === '' || humanas === '' || natureza === '' || mate === '' || redação === ''){
+       window.alert("preencha todas as lacunas");
+       return;
+    }
+
+    let ling = Number(linguagem)
+    let hum = Number(humanas)
+    let nat = Number(natureza)
+    let mat = Number(mate)
+    let red = Number(redação)
 
     let media = ((ling + hum + nat + mat + red) / 5)
 
-       
-    if(media <= 560){
-        resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
-        img.setAttribute('src', 'img/imagemBaixa.jpg')
-    } else if (media <= 660){
-        resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
-        img.setAttribute('src', 'img/imagemRuim.jpg')
-    } else if (media <= 760){
-        resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
-        img.setAttribute('src', 'img/imagemModerada.jpg')
-    } else {
-        resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
-        img.setAttribute('src', 'img/imagemBoa.jpg')
-    }
-resultado.style.textAlign = 'center'
-resultado.appendChild(img)
+        if(media <= 560){
+            resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
+            img.setAttribute('src', 'img/imagemBaixa.jpg')
+        } else if (media <= 660){
+            resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
+            img.setAttribute('src', 'img/imagemRuim.jpg')
+        } else if (media <= 760){
+            resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
+            img.setAttribute('src', 'img/imagemModerada.jpg')
+        } else {
+            resultado.innerHTML = `Sua média simples é: ${media.toFixed(2)}`
+            img.setAttribute('src', 'img/imagemBoa.jpg')
+        }
+    resultado.style.textAlign = 'center'
+    resultado.appendChild(img)
+    
     
 }
 
 function CalcularCota(){
-    let linguagem = document.getElementById('linguagem')
-    let humanas = document.getElementById('humanas')
-    let natureza = document.getElementById('natureza')
-    let mate = document.getElementById('mate')
-    let redação = document.getElementById('redação')
+    let linguagem = document.getElementById('linguagem').value.trim();
+    let humanas = document.getElementById('humanas').value.trim();
+    let natureza = document.getElementById('natureza').value.trim();
+    let mate = document.getElementById('mate').value.trim();
+    let redação = document.getElementById('redação').value.trim();
     let resultadoCota = document.getElementById('resultadoCota')
     let img = document.createElement('img')
     img.setAttribute('id', 'foto')
 
-    let ling = Number(linguagem.value)
-    let hum = Number(humanas.value)
-    let nat = Number(natureza.value)
-    let mat = Number(mate.value)
-    let red = Number(redação.value)
+    if(linguagem === '' || humanas === '' || natureza === '' || mate === '' || redação === ''){
+        window.alert("preencha todas as lacunas");
+        return;
+     }
+
+     let ling = Number(linguagem)
+     let hum = Number(humanas)
+     let nat = Number(natureza)
+     let mat = Number(mate)
+     let red = Number(redação)
 
     let media = ((ling + hum + nat + mat + red) / 5) * (1.1)
      
